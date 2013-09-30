cba647d286c55bd61252115d9a7898e8cf5d7b9d
patrizio <patrizio.se@gmail.com>
2013-10-01 01:16:29 +0200

    Updated about page

--- themes/bootstrap/views/site/pages/about.php
+++ themes/bootstrap/views/site/pages/about.php
@@ -6,5 +6,11 @@ $this->breadcrumbs  = array(
     'About',
 );
 ?>
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<p>
+    This is micro application written in <a target="_blank" href="http://www.php.net">PHP</a>, on the <a target="_blank" href="http://www.yiiframework.com">Yii Framework</a> for manage bookmarks.</p>
+    The design is simple, the UI clear. You can add an URL and search it in the list.
+    The application get an URL, scrape the HTML page and the favicon and saves them on the database.
+</p>
+   <p>
+    It's incomplete (I made it in 3 days) but was built only for demo purpose for the <strong>Istituto Italiano di Tecnologia.</strong>
+</p>
