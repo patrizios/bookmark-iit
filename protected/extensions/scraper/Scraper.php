0a3dfb56b306abe60e49bc37fcc099fa866c6c6d
patrizio <patrizio.se@gmail.com>
2013-09-30 18:36:57 +0200

    Created new Scraper classes

---
+++ protected/extensions/scraper/Scraper.php
@@ -0,0 +1,289 @@
+<?php
+/**
+ *
+ */
+class Scraper
+{
+    /**
+     * [$maxredirects]
+     * Http client max redirects
+     * @var integer
+     */
+    public $maxredirects = 0;
+
+    /**
+     * [$timeout]
+     * Http client timeout
+     * @var integer
+     */
+    public $timeout = 30;
+
+    /**
+     * [$permitted_content_types]
+     * Permitted content types
+     * @var array
+     */
+    public $permitted_content_types = array(
+        'text/html' => 'html'
+    );
+
+    private $_url;
+    private $_client;
+    private $_response;
+
+    /**
+     * [__construct]
+     *
+     * @param string $url Url to scrape
+     */
+    public function __construct($url)
+    {
+        $this->setUrl($url);
+        $this->_run();
+    }
+
+    /**
+     * [_run]
+     * Run the scrape operations
+     *
+     * @return null
+     */
+    private function _run()
+    {
+        $this->_initClient();
+        $this->sendRequest();
+
+        $response = $this->getResponse();
+
+        if (!$response->isSuccessful()) {
+            throw new ScraperRequestErrorException();
+        }
+
+        if (!$this->isPermittedContentType()) {
+            throw new ScraperContentNotPermittedException();
+        }
+    }
+
+    /**
+     * [_initClient]
+     * Init the client object
+     *
+     * @return null
+     */
+    private function _initClient()
+    {
+        if (!$this->getClient()) {
+            $this->_client = new EHttpClient(
+                $this->_url,
+                array(
+                    'maxredirects' => $this->maxredirects,
+                    'timeout'      => $this->timeout
+                )
+            );
+        }
+    }
+
+    /**
+     * [setUrl]
+     *
+     * @param string $url Set uri
+     *
+     * @throws If not valid url
+     * @return null
+     */
+    public function setUrl($url)
+    {
+        if ($this->isValidUrl($url)) {
+            $this->_url = $url;
+        } else {
+            throw new ScraperInvalidUrlException();
+        }
+    }
+
+    /**
+     * [getUrl]
+     *
+     * @return string Url property
+     */
+    public function getUrl()
+    {
+        return $this->_url;
+    }
+
+    /**
+     * [getClient]
+     *
+     * @return EHttpClient Http client object
+     */
+    public function getClient()
+    {
+        return $this->_client;
+    }
+
+    /**
+     * [sendRequest]
+     * Make the client send the request
+     * Set the response property
+     *
+     * @return null
+     */
+    public function sendRequest()
+    {
+        $client             = $this->getClient();
+        $this->_response    = $client->request();
+    }
+
+    /**
+     * [getResponse]
+     *
+     * @return EHttpResponse Return the saved response
+     */
+    public function getResponse()
+    {
+        return $this->_response;
+    }
+
+    /**
+     * [getResponseBody]
+     *
+     * @return string The response body
+     */
+    public function getResponseBody()
+    {
+        return $this->_response->getBody();
+    }
+
+    /**
+     * [getResponseHeaders]
+     * Get all response headers
+     *
+     * @return mixed Response HTTP headers, NULL if no Response
+     */
+    public function getResponseHeaders()
+    {
+        if ($this->_response) {
+            return $this->_response->getHeaders();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * [getResponseHeader]
+     * Get a specific response header
+     *
+     * @param string $name Header name
+     *
+     * @return mixed Response HTTP headers, NULL if no Response
+     */
+    public function getResponseHeader($name)
+    {
+        if ($this->getResponseHeaders()) {
+            $headers = $this->getResponseHeaders();
+
+            if (isset($headers[$name])) {
+                return $headers[$name];
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * [getResponseContenType]
+     *
+     * @return string response content type
+     */
+    public function getResponseContenType()
+    {
+        $content_type = $this->getResponseHeader('Content-type');
+
+        if ($content_type) {
+
+            $pos = strpos($content_type, ';');
+
+            if ($pos !== false) {
+                $ct_exploded    = explode(';', $content_type);
+                $content_type   = $ct_exploded[0];
+            }
+
+            $content_type = strtolower($content_type);
+        }
+
+        return $content_type;
+    }
+
+    /**
+     * [isPermittedContentType]
+     * Check if the response have a permitted content type
+     *
+     * @return boolean [description]
+     */
+    public function isPermittedContentType()
+    {
+        $content_type = $this->getResponseContenType();
+
+        if ($content_type) {
+            $permitted_types = array_keys($this->permitted_content_types);
+            return in_array($content_type, $permitted_types);
+        }
+
+        return false;
+    }
+
+    /**
+     * [isValidUrl]
+     * Check if a string is a valid url
+     *
+     * @param string $url Url to check
+     *
+     * @return boolean TRUE if valid url, FALSE if not
+     */
+    public static function isValidUrl($url)
+    {
+        // scheme
+        $regex = "^(https?|ftp)\:\/\/";
+
+        // user and pass (optional)
+        // $re .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?";
+
+        // http://x.xx(x) = minimum
+        $regex .= "([a-z0-9+\$_-]+\.)*[a-z0-9+\$_-]{2,3}";
+
+        // port (optional)
+        $regex .= "(\:[0-9]{2,5})?";
+
+        // path (optional)
+        $regex .= "(\/([a-z0-9+\$_-]\.?)+)*\/?";
+
+        return preg_match("/$regex/siU", $url);
+    }
+}
+/**
+ * Generic Scraper Exception
+ */
+class ScraperException extends Exception { }
+
+/**
+ * Invalid URL Exception
+ */
+class ScraperInvalidUrlException extends ScraperException
+{
+    protected $message = 'Invalid URL';
+}
+
+/**
+ * Request Error Exception
+ */
+class ScraperRequestErrorException extends ScraperException
+{
+    protected $message = 'Request Error';
+}
+
+/**
+ * Content not permitted Exception
+ */
+class ScraperContentNotPermittedException extends ScraperException
+{
+    protected $message = 'Content not permitted';
+}
---
+++ protected/extensions/scraper/ScraperHtml.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * Specialized Scraper for HTML page
+ */
+class ScraperHtml extends Scraper
+{
+    /**
+     * [$permitted_content_types]
+     * Permitted content types
+     * @var array
+     */
+    public $permitted_content_types = array(
+        'text/html' => 'html'
+    );
+
+    private $_dom_document;
+    private $_head_meta = array();
+    private $_head_link = array();
+
+    /**
+     * [__construct]
+     *
+     * @param string $url Url to scrape
+     */
+    public function __construct($url)
+    {
+        parent::__construct($url);
+
+        if ($this->isHtml()) {
+            $this->initDomDocument($this->getResponseBody());
+        }
+    }
+
+    /**
+     * [initDomDocument]
+     *
+     * @param string $html HTML document
+     *
+     * @return null
+     */
+    public function initDomDocument($html)
+    {
+        $this->_dom_document = new DOMDocument();
+        @$this->_dom_document->loadHTML($html);
+    }
+
+    /**
+     * [isHtml]
+     * Check if content type of the response is HTML
+     *
+     * @return boolean TRUE if HTML, FALSE if not
+     */
+    public function isHtml()
+    {
+        $content_type = $this->getResponseContenType();
+
+        if ($content_type == 'text/html') {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * [getTitle]
+     * Get the title of the HTML document
+     *
+     * @return string Document title
+     */
+    public function getTitle()
+    {
+        $nodes = $this->_dom_document->getElementsByTagName('title');
+        return $nodes->item(0)->nodeValue;
+    }
+
+    /**
+     * [getMetas]
+     * Get the meta tags of the HTML document
+     *
+     * @return array Meta tags array
+     */
+    public function getMetas()
+    {
+        $dom_metas = $this->_dom_document->getElementsByTagName('meta');
+
+        for ($i = 0; $i < $dom_metas->length; $i++) {
+
+            $meta = $dom_metas->item($i);
+
+            $attribute_name     = $meta->getAttribute('name');
+            $attribute_content  = $meta->getAttribute('content');
+
+            $this->_head_meta[$attribute_name] = $attribute_content;
+        }
+
+        return $this->_head_meta;
+    }
+
+    /**
+     * [getMeta]
+     * Get a specific meta tag
+     *
+     * @param string $name Meta tag name
+     *
+     * @return array Meta tag array
+     */
+    public function getMeta($name)
+    {
+        $meta = $this->getMetas();
+
+        if (isset($meta[$name])) {
+            return $meta[$name];
+        }
+
+        return null;
+    }
+
+    /**
+     * [getHeadLinks]
+     * Get all the head links of the HTML document
+     *
+     * @return array Head links array
+     */
+    public function getHeadLinks()
+    {
+        $dom_links = $this->_dom_document->getElementsByTagName('link');
+
+        for ($i = 0; $i < $dom_links->length; $i++) {
+
+            $link = $dom_links->item($i);
+
+            $attribute_rel  = $link->getAttribute('rel');
+            $attribute_type = $link->getAttribute('type');
+            $attribute_href = $link->getAttribute('href');
+
+            $this->_head_link[$attribute_rel][] = array(
+                'type'  => $attribute_type,
+                'href'  => $attribute_href
+            );
+        }
+
+        return $this->_head_link;
+    }
+
+    /**
+     * [getHeadLinksByRel]
+     * Get a specific type of head links by rel
+     *
+     * @param string $rel Rel of head links
+     *
+     * @return array Head links array
+     */
+    public function getHeadLinksByRel($rel)
+    {
+        $links = $this->getHeadLinks();
+
+        if (isset($links[$rel])) {
+            return $links[$rel];
+        }
+
+        return null;
+    }
+}
