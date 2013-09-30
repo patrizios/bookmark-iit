5eb379da18592ad4c2e8eddb8fde44c0850ecc25
patrizio <patrizio.se@gmail.com>
2013-10-01 00:54:48 +0200

    Fixed permission to capcha action

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -27,7 +27,7 @@ class SiteController extends Controller
                 'users'=>array('*'),
             ),
             array('allow', // allow authenticated user to perform 'create' and 'update' actions
-                'actions'=>array('page', 'contact', 'logout'),
+                'actions'=>array('page', 'contact', 'logout', 'captcha'),
                 'users'=>array('@'),
             ),
             array('allow', // allow admin user to perform 'admin' and 'delete' actions
